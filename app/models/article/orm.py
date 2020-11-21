ee6fb6926b8f48399c12e194840dd723775876b4
normidar <1106408182@qq.com>
2020-11-21 13:18:18 +0900

    u

--- app/models/article/orm.py
+++ app/models/article/orm.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class ArticleBase(BaseModel):
     title:           Optional[str] = None
     content:         Optional[str] = None
-    seo_meta:        Optional[str] = None
+    description:     Optional[str] = None
     seo_title:       Optional[str] = None
     seo_keywords:    Optional[str] = None
     seo_description: Optional[str] = None
