fb0bad0fc4536eec610f680d686cf8fde907c341
normidar <1106408182@qq.com>
2020-11-27 19:12:36 +0900

    u

--- app/models/editor/route.py
+++ app/models/editor/route.py
@@ -4,8 +4,8 @@ from app.main import check_token
 from app.models.user.user import User
 bp = APIRouter()
 
-@bp.post('/write/{file_path:path}',description='写')
-def write(file_path: str, data: str=Body(...)):
+@bp.post('/write',description='写')
+def write(file_path: str=Body(...), data: str=Body(...)):
     if file_path[0] != '/':
         return crud.write(file_path, data)
     else:
@@ -15,15 +15,15 @@ def write(file_path: str, data: str=Body(...)):
 def ls():
     return crud.ls()
 
-@bp.get('/read/{file_path:path}',description='读取文件内容')
+@bp.get('/read',description='读取文件内容')
 def read(file_path: str):
     if file_path[0] != '/':
         return crud.read(file_path)
     else:
         raise HTTPException(status_code=422,detail='斜杠太多')
 
-@bp.post('/create/{directory:path}',description='创建文件夹路径')
-def create(directory: str):
+@bp.post('/create',description='创建文件夹路径')
+def create(directory: str=Body(...,embed=True)):
     if directory[0] != '/':
         if crud.check_path(directory):
             raise HTTPException(status_code=409,detail='文件或文件夹已存在')
