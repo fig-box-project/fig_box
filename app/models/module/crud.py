ef973a603cca45dc026f7b0a6746652ca9a5335f
normidar <36730656+normidar@users.noreply.github.com>
2020-12-04 11:02:15 +0900

    Update crud.py

--- app/models/module/crud.py
+++ app/models/module/crud.py
@@ -13,7 +13,7 @@ def install_module(module:orm.ModuleInstall):
             cofig_posi['head'] = i
         elif lines[i] == '# <for modules\n':
             cofig_posi['foot'] = i
-    return config_posi
+    return cofig_posi
 
 def use_module(module:orm.ModuleUse):
     pass
