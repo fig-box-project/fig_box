9c3e6315368edc866f4c305588b8085b4400edd8
normidar <36730656+normidar@users.noreply.github.com>
2020-12-04 10:13:47 +0900

    Update orm.py

--- app/models/module/orm.py
+++ app/models/module/orm.py
@@ -3,8 +3,17 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class ModuleBase(BaseModel):
+    module_name: str
+
+# 自動使用
+class ModuleInstall(ModuleBase):
+    module_version:int = 1
+        
+class ModuleUninstall(ModuleBase):
+    pass
+        
+class ModuleUse(ModuleBase):
     pass
 
-class ModuleInstall(ModuleBase):
-    module_name: str
-    module_version:int = 1
+class ModuleUnuse(ModuleBase):
+    pass
