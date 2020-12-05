46d991aef84fd252da826dc15b50030793f16658
normidar <1106408182@qq.com>
2020-12-05 19:29:45 +0900

    u

--- app/models/comment/route.py
+++ app/models/comment/route.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends,Header,Body, Request
-from . import crud, orm
+# from . import crud, orm
 from app.main import check_token
 from app.models.user.mdl import User
 bp = APIRouter()
