5c29433b21a26ce605c52e040e7b4218b9f2c2d7
劉達 <ryu@anchorz.co.jp>
2021-01-10 23:43:28 +0900

    u

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ from typing import Optional
 from sqlalchemy.orm import Session
 from sqlalchemy.orm import sessionmaker
 from app.models import database
-from fastapi import FastAPI,Depends,Header,HTTPException
+from fastapi import FastAPI,Depends,Header,HTTPException,Request
 from fastapi.responses import HTMLResponse
 from starlette.middleware.cors import CORSMiddleware # CORS 中间件
+
 
 # 引用一下mdl才能创建该数据表
 from app.models.user import mdl as user
@@ -127,8 +128,8 @@ app.include_router(
 # <for modules
 
 @app.get('/',tags=['测试'],response_class=HTMLResponse)
-def root():
-    return "<a href=\"docs\">api</a>"
+def root(request: Request):
+    return "<a href=\"docs\">api</a>" + request.client.host
 
 
 
