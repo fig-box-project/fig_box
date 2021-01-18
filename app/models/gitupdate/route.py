a6ebfeb2f374a78940dcaa4301562fe06b4fa493
劉達 <ryu@anchorz.co.jp>
2021-01-18 10:32:27 +0900

    u

--- app/models/gitupdate/route.py
+++ app/models/gitupdate/route.py
@@ -5,20 +5,12 @@ bp = APIRouter()
 
 @bp.post("/listen")
 def read(data = Body(...)):
-    codes = [
-        "date",
-        "git pull"
-    ]
     commit_message = data["commits"][0]["message"]
     print(commit_message)
     # 如果消息为d则删库
-    d_codes = [
-        "rm -f db.sqlite"
-    ]
     if commit_message == "d":
-        for code in d_codes:
-            request = os.popen(code)
-            # print(request.read())
+        request = os.popen("rm -f db.sqlite")
+        print(request.read())
     # pull的执行
     request = os.popen("git pull")
     print(request.read().split("\n")[-2])
