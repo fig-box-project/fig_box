922e5cdb1d48c6220fa7885d076cb96a72e156d0
劉達 <ryu@anchorz.co.jp>
2021-01-31 11:55:34 +0900

    把article脱离

--- app/main.py
+++ app/main.py
@@ -58,7 +58,7 @@ if db.query(user.User).count() == 0:
 app = FastAPI(
     title = "F-Mod",
     description = "这是自由、易管理的高速模组化cms system.",
-    version = "α8"
+    version = "α9"
 )
 
 # 进入路由时检测token
