d33b5761c5419858089de1be96342eb17614959a
劉達 <ryu@anchorz.co.jp>
2021-02-06 20:59:23 +0900

    删除旧的sitemap代码

--- app/models/render/crud.py
+++ app/models/render/crud.py
@@ -85,25 +85,3 @@ def view_list(db: Session,request,begin_id:int,length:int):
 def render_sitemap():
     return FileResponse('files/sitemap/sitemap.xml',media_type='application/xml')
     # return templates.TemplateResponse(templates_path + '/sitemap.xml',{},media_type='application/xml')
-
-def create_sitemap(db: Session):
-    fields = ['link']
-    datas = db.query(mdl.Article).options(load_only(*fields)).all()
-    lines = []
-    first_line = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n'
-    lines.append(first_line)
-    for i in datas:
-        # print(i.link)
-        code = \
-f'''<url>
-<loc>{conf.sitemap_url}/article/{i.link}</loc>
-<priority>1.00</priority>
-<lastmod>2020-12-09</lastmod>
-<changefreq>daily</changefreq>
-</url>
-'''
-        lines.append(code)
-    last_line = '</urlset>'
-    lines.append(last_line)
-    with open('files/sitemap/sitemap.xml','w') as w:
-        w.write(''.join(lines))
