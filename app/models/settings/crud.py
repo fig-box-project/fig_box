e6804451f377bf3cf67a766784ec410abf66d4c9
劉達 <ryu@anchorz.co.jp>
2021-02-14 11:28:43 +0900

    u 修正git无法pull的问题

--- app/models/settings/crud.py
+++ app/models/settings/crud.py
@@ -1,3 +1,4 @@
+from shutil import copyfile
 import yaml
 import os
 
@@ -5,11 +6,11 @@ class Settings:
     def __init__(self):
         self.yaml_path = "settings.yml"
         if not os.path.exists(self.yaml_path):
-            raise Exception("Settings.yml not found")
-        else:
-            # 获取设置
-            with open(self.yaml_path, "r") as f:
-                self.value: dict = yaml.load(f, Loader=yaml.SafeLoader)
+            # 复制默认文件
+            copyfile("settings_default.yml", "settings.yml")
+        # 获取设置
+        with open(self.yaml_path, "r") as f:
+            self.value: dict = yaml.load(f, Loader=yaml.SafeLoader)
     
     def update(self):
         with open(self.yaml_path, "w") as f:
