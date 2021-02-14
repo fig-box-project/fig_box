d91cc1ce0b7f60dbf3f6edafa5a4f891de3607c2
劉達 <ryu@anchorz.co.jp>
2021-02-14 11:08:16 +0900

    u

--- app/models/gitupdate/route.py
+++ app/models/gitupdate/route.py
@@ -20,8 +20,7 @@ def read(data = Body(...)):
     # 备份各样东西
     packager.dump()
     # pull的执行
-    request = os.popen("git fetch --all")
-    request = os.popen("git reset --hard origin/master")
+    request = os.popen("git pull --ff-only")
     
     
 @bp.post("/blog",description="TODO:拒绝从github以外的来源")
