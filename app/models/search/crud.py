66fe001d91130abe7dd115f0027f432af4524d36
劉達 <ryu@anchorz.co.jp>
2021-02-24 21:22:01 +0900

    u

--- app/models/search/crud.py
+++ app/models/search/crud.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import load_only
+from app.models.settings.crud import settings
 
 # 数据库搜索,
 def db_search(db, db_name, type, count, params:list):
@@ -18,6 +19,11 @@ def db_search(db, db_name, type, count, params:list):
         elif len(params) == 2:
             fields = ['title', 'link', 'description']
             rt = eval(f"db.query({bigFirst}).options(load_only(*fields)).filter({bigFirst}.category_id == params[0]).limit(count).offset((int(params[0])-1)*count).all()")
+        change_link_to_abs(rt)
         return rt
     else:
         return None
+
+def change_link_to_abs(rt):
+    for i in rt:
+        i["link"] = settings.value["domain_port"] + "/article/show/" + i["link"]
