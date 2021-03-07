654825284405100cb21e866c64e00222f8131b9f
劉達 <ryu@anchorz.co.jp>
2021-03-07 16:16:47 +0900

    u

--- app/models/assets/crud.py
+++ app/models/assets/crud.py
@@ -1,8 +1,13 @@
+import os
+from fastapi import HTTPException
 
 class Assets:
     @staticmethod
-    def insert(asset, path, owner_id, visibility = True, limit = 0):
+    def insert(asset, path:str, owner_id, visibility = True, limit = 0):
         # limit为0 时为无限制
         asset_len = len(asset)
-        if limit != 0:
-            
+        if limit != 0 and asset_len > limit * 1000000:
+            raise HTTPException(404,"上传文件的大小超过系统限制")
+        while os.path.exists("files/assets/" + path):
+            i = path.rfind(".")
+            path = path[:i]
