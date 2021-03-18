0b8d3f0c866391749975bc70720cc0aed57105a9
劉達 <ryu@anchorz.co.jp>
2021-03-18 12:48:12 +0900

    图片算为资源

--- app/models/assets/crud.py
+++ app/models/assets/crud.py
@@ -1,30 +1,58 @@
 import os
 from fastapi import HTTPException
 from app.models.user.mdl import User
+from app.models.settings.crud import settings
+
+path_prefix = "files/assets/"
+allow_upload_type = {"jpg", "png", "bmp"}
 
 class Assets:
+    
     @staticmethod
-    def insert(asset, path:str, owner:User, visibility = True, limit = 0):
-        # limit为0 时为无限制
-        asset_len = len(asset)
-        if limit != 0 and asset_len > limit * 1000000:
-            raise HTTPException(404,"上传文件的大小超过系统限制")
-        # 对于文件已存在时的处理
-        if os.path.exists(f"files/assets/{User.id}/{path}"):
-            # 分头尾
-            i = path.rfind(".")
-            path_head = path[:i]
-            path_foot = path[i:]
-            # 当存在时更改名称再插入
-            for j in range(99):
-                path = f"{path_head}_{j}{path_foot}"
-                if os.path.exists(f"files/assets/{User.id}/{path}"):
-                    continue
-                with open(f"files/assets/{User.id}/{path}", 'wb') as f:
+    def get_link_prefix():
+        # 获取链接前缀
+        return settings.value["domain_port"] + "/assets"
+
+    @staticmethod
+    def insert_file(asset, path: str, filename: str, mode = "auto_countup",limit = 0):
+        # 插入
+        # 分头尾
+        i = filename.rfind(".")
+        filename_head = filename[:i]
+        filename_foot = filename[i:]
+        # 不允许上传的类型要截断
+        if filename_foot not in allow_upload_type:
+            raise HTTPException(403, "Upload type not allowed")
+        if mode == "auto_countup":
+            # limit为0 时为无限制
+            asset_len = len(asset)
+            if limit != 0 and asset_len > limit * 1000000:
+                raise HTTPException(404,"上传文件的大小超过系统限制")
+            # 对于文件已存在时的处理
+            if os.path.exists(f"{path_prefix}{path}/{filename}"):
+                # 当存在时更改名称再插入
+                for j in range(99):
+                    filename = f"{filename_head}_{j}{filename_foot}"
+                    if os.path.exists(f"{path_prefix}{path}/{filename}"):
+                        continue
+                    with open(f"{path_prefix}{path}/{filename}", 'wb') as f:
+                        f.write(asset)
+                    break
+            else:
+                # 不存在时,直接插入
+                with open(f"{path_prefix}{path}/{filename}", 'wb') as f:
                     f.write(asset)
-                break
+            return (f"{path_prefix}{path}/{filename}", filename, asset_len)
         else:
-            # 不存在时,直接插入
-            with open(f"files/assets/{User.id}/{path}", 'wb') as f:
-                f.write(asset)
-        return path
+            raise HTTPException(500,"模式不支持")
+
+    @staticmethod
+    def insert_with_user(asset, filename:str, owner:User, visibility = True, limit = 0):
+        rt = Assets.insert_file(asset, f"user/{owner.id}", filename, limit = limit)
+        return {"link":f"user/{owner.id}/{rt[1]}", "size":rt[2]}
+
+
+
+    @staticmethod
+    def insert_with_character():
+        ...
--- app/models/photo/route.py
+++ app/models/photo/route.py
@@ -1,3 +1,4 @@
+from app.models.assets.crud import Assets
 from fastapi import APIRouter, HTTPException, Body, Request, File,UploadFile, Depends
 from . import crud, orm
 from app.main import check_token
@@ -6,13 +7,7 @@ from app.models.user.mdl import User
 bp = APIRouter()
 
 @bp.post('/upload',description = '上传文件')
-async def upload(name: str = Body(...),now_user:User = Depends(check_token), file: UploadFile = File(...)):
-    rt = await crud.create_file(file,name)
-    if rt == None:
-        raise HTTPException(status_code=404,detail='已存在图片')
-    elif rt == "over":
-        raise HTTPException(status_code=406,detail="图片过大")
-    elif rt == "false":
-        raise HTTPException(status_code=406,detail="格式不正确")
-    else:
-        return rt
+async def upload(name: str = Body(...),now_user:User = Depends(check_token), photo: UploadFile = File(...)):
+   rt = Assets.insert_with_user(photo, name, now_user, limit = 5)
+   return rt
+   
