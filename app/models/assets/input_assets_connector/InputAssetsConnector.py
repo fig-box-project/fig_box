acf131a80c0cb7eb0d93a996620b6b7c4201dfb7
劉達 <ryu@anchorz.co.jp>
2021-04-08 10:55:46 +0900

    u

--- app/models/assets/input_assets_connector/InputAssetsConnector.py
+++ app/models/assets/input_assets_connector/InputAssetsConnector.py
@@ -5,9 +5,14 @@ class InputAssetsConnector():
     root_path:str = 'files/assets/'
     mode = 'auto_count_up'
     limit:int = 0
+    size:int = 0
     def __init__(self, path: str, filename: str):
         self.path = path
         self.filename = filename
+
+    # 用于被重写,被外部调用
+    async def packup(self):
+        ...
 
     def update_filename(self):
         # 用于在命名冲突时改变文件名
@@ -16,7 +21,6 @@ class InputAssetsConnector():
         else:
             raise HTTPException(500,"模式不支持")
 
-    # TODO
     def __auto_count_up(self):
         # 文件名后加_[数字]来解决冲突
         # 分头尾
@@ -24,25 +28,23 @@ class InputAssetsConnector():
         filename_head = self.filename[:i]
         filename_foot = self.filename[i:]
         index = 0
-        while os.path.exists(self.__get_full_path()):
+        while os.path.exists(self.get_full_path()):
+            # 合头尾
             self.filename = f"{filename_head}_{str(index)}{filename_foot}"
             index += 1
-            
-
         
-
     def check_assert(self, asset):
         # 检查文件大小
-        asset_len = len(asset)
-        if self.limit != 0 and asset_len > self.limit * 1000000:
+        self.size = len(asset)
+        if self.limit != 0 and self.size > self.limit * 1000000:
             raise HTTPException(403,"文件的大小超过系统限制")
     
     def save_asset(self, asset):
         # 保存资源
-        full_path = self.__get_full_path()
+        full_path = self.get_full_path()
         print("保存的位置:" + full_path)
         with open(full_path, 'wb') as f:
             f.write(asset)
 
-    def __get_full_path(self):
-        return f"{self.root_path}{self.path}{self.filename}"
+    def get_full_path(self):
+        return f"{self.root_path}{self.path}/{self.filename}"
--- app/models/assets/input_assets_connector/InputUploadConnector.py
+++ app/models/assets/input_assets_connector/InputUploadConnector.py
@@ -1,15 +1,26 @@
 from .InputAssetsConnector import InputAssetsConnector
+from fastapi import HTTPException
 from fastapi.datastructures import UploadFile
 
 class InputUploadConnector(InputAssetsConnector):
+    # 允许上传的文件类型
+    allow_upload_type = {"jpg", "png", "bmp"}
+
     def __init__(self, path: str, filename: str, file: UploadFile, limit: int = 5):
         super(InputUploadConnector, self).__init__(path, filename)
         self.__file = file
         self.limit = limit
 
+    # 打包资源到文件
     async def packup(self):
-        # 打包资源到文件
+        super(InputUploadConnector, self).packup()
+        self.__check_assert_type()
         self.update_filename()
         asset = await self.__file.read()
         self.check_assert(asset)
         self.save_asset(asset)
+
+    # 检查后缀能否上传
+    def __check_assert_type(self):
+        if self.filename[self.filename.rfind(".")+1:] not in self.allow_upload_type:
+            raise HTTPException(403, "Upload type not allowed")
