daa7c90e1fe9d855e8c94a7bb01bdae2a71c6b0f
劉達 <ryu@anchorz.co.jp>
2021-04-18 15:16:03 +0900

    44

--- app/models/user/mdl.py
+++ app/models/user/mdl.py
@@ -1,38 +1,41 @@
-from fastapi import APIRouter, HTTPException, Depends,Header
+from fastapi import APIRouter, HTTPException, Depends, Header
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 from app.models.mdl.database import Base
 from app.models.character.crud import check_auth
 from app.models.settings.crud import settings
-import jwt,time
+import jwt
+import time
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class User(Base):
     __tablename__ = 'users'
-    id              = Column(Integer, primary_key=True)
-    username        = Column(String(32), index=True)
-    password_hash   = Column(String(128))
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(128))
 
-    character      = Column(String(64), default=2)
-    birth_date     = Column(DateTime)
+    character = Column(String(64), default=2)
+    birth_date = Column(DateTime)
 
-    def get_token(self,expires_in=3600):
+    def get_token(self, expires_in=3600):
         try:
             data = jwt.encode(
-                {'id':self.id, 'exp':time.time()+expires_in},
+                {'id': self.id, 'exp': time.time()+expires_in},
                 settings.value['token_key'], algorithm='HS256')
         except:
             return "error"
         return data
-    
+
     # 检查权限,auth请输入权限符
-    def into_auth(self,auth:str):
-        if not check_auth(self.character,auth):
-            raise HTTPException(status_code=403,detail='用户权限不足,不能进入 ' + auth + ' 权限')
-    
+    def into_auth(self, auth: str):
+        if not check_auth(self.character, auth):
+            raise HTTPException(
+                status_code=403, detail='用户权限不足,不能进入 ' + auth + ' 权限')
+
     def hash_password(self, password):
         self.password_hash = generate_password_hash(password)
-    
+
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
