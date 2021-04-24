e502db359b7ab3257c7ddf5d1c27349c72b8d377
劉達 <ryu@anchorz.co.jp>
2021-04-24 20:42:11 +0900

    u

--- app/models/category/mdl.py
+++ app/models/category/mdl.py
@@ -1,12 +1,9 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 from app.models.mdl.page import PageMdl
-from app.models.mdl.hasid import HasidMdl
-from app.models.mdl.database import Base
-# from app.models.mdl.hasid import PageMdl
+
 
 class Category(PageMdl):
     __tablename__ = 'category'
     # 暂时无用
-    father_ids         = Column(String) 
-    
+    father_ids = Column(String)
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run("debug_server:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, ssl_keyfile='')
