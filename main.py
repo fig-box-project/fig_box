61305457d0a2126906a785427dbf67129f90bce3
劉達 <ryu@anchorz.co.jp>
2021-07-13 00:20:50 +0900

    变更到8080

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True, ssl_keyfile='')
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, ssl_keyfile='')
