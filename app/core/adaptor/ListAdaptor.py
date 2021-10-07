70d51296306375e7357ec6fbacf1146dc328937a
劉達 <normidar7@gmail.com>
2021-10-07 23:52:56 +0900

    リストアダプターにmap_filter機能を入れる

--- app/core/adaptor/ListAdaptor.py
+++ app/core/adaptor/ListAdaptor.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import Type, Callable
 
 from fastapi import Depends
 from sqlalchemy import func
@@ -14,10 +14,18 @@ class ListAdaptor:
         self.page_size = page_size
         self.__db = db
 
-    def search(self, table_class: Type[HasIdTable]) -> dict:
+    def search(self, table_class: Type[HasIdTable], map_filter: Callable = None) -> dict:
+        """ map_filter is a function to filter all the data, it has one input and one output,
+        it means old_data and new_data.
+         map_filterは関数です、一個パdirtのデータが入る、結果（新しいデータ）をリターンしてください"""
         count = self.__db.query(func.count(table_class.id)).scalar()
         data = self.__db.query(table_class).offset((self.page_index - 1) * self.page_size) \
             .limit(self.page_size).all()
+        if map_filter is not None:
+            old_data = data
+            data = []
+            for i in old_data:
+                data.append(map_filter(i.get_dict()))
         return {
             'data': data,
             'all_count': count,
