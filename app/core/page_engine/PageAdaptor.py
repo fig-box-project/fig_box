4c2f52a0050af523d6cd8c0c8e319118a37ab2b6
劉達 <normidar7@gmail.com>
2022-02-25 13:34:35 +0900

    fix AttributeError: 'Query' object has no attribute 'get_filters'

--- app/core/page_engine/PageAdaptor.py
+++ app/core/page_engine/PageAdaptor.py
@@ -1,12 +1,11 @@
 from typing import Type
 
-from fastapi import Request, Depends
+from app.core.database_engine.db_core import get_db
+from app.core.table_class import PageTable
+from app.core.template_engine.Template import Template
+from fastapi import Depends, Request
 from sqlalchemy.orm import Session
 from starlette.responses import Response
-
-from app.core.table_class import PageTable
-from app.core.database_engine.db_core import get_db
-from app.core.template_engine.Template import Template
 
 
 class PageAdaptor:
@@ -19,7 +18,8 @@ class PageAdaptor:
         :template_path
             相対的なテンプレートパスを入れる（モジュール名も入れる）、例えば：/sample/sample.html
         """
-        page_row = self.db.query(table_class).get_filters(table_class.link == link).first()
+        page_row = self.db.query(table_class).filter(
+            table_class.link == link).first()
         if page_row is None:
             return Template.response_404(self.request, '404 cannot find data in database')
         # get a dict from row
